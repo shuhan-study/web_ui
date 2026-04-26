522b0ab5f9ae4ec89c7ef06cba0a84a3640ef20b
capable <rj.geng@gmail.com>
2026-04-26 10:00:44 -0700

    B4: Navbar Grades link before About (wu-n6m)

--- web/components/navbar/Navbar.tsx
+++ web/components/navbar/Navbar.tsx
@@ -9,6 +9,7 @@ function Navbar() {
       <Container className='flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap gap-4 py-8'>
         <Logo />
         <div className='flex gap-4 items-center'>
+          <NavLink href='/grades'>Grades</NavLink>
           <NavLink href='/about'>About</NavLink>
           <DarkMode />
         </div>
